6171cfcca0ed24b1271bd831d62b0998464fb941
Raman attri <raman.kumar.1232004@gmail.com>
2025-10-25 17:17:33 +0530

    Binary Search 704

---
+++ DSA3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class DSA3 {
+    public static void main (String args[]){
+        int number[] = {3,4,5,6,7,8};
+        int key = 8;
+       System.out.println("Key is :"+  binarySearch(number, key));
+    }
+    public static int binarySearch(int number[] , int key){
+        int start = 0 , end = number.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(number[mid]==key){
+                return mid;
+            }
+            if(number[mid]>key){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+}
