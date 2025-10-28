93ca8c58f562da57790b29fffaee7246ace3bd40
Raman attri <raman.kumar.1232004@gmail.com>
2025-10-29 03:14:21 +0530

    LargestNumber

--- Arrays.java
+++ Arrays.java
@@ -1,20 +1,21 @@
 public class Arrays {
     public static void main(String args[]) {
         int arr[] = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        k %= arr.length;
+        // int k = 3;
+        // k %= arr.length;
 
        
-        rotate(arr, 0, arr.length - 1);
+        // rotate(arr, 0, arr.length - 1);
        
-        rotate(arr, 0, k - 1);
+        // rotate(arr, 0, k - 1);
       
-        rotate(arr, k, arr.length - 1);
+        // rotate(arr, k, arr.length - 1);
 
-        // Print rotated array 
-        for(int ele : arr){
-            System.out.print(ele + " ");
-        }
+        // // Print rotated array 
+        // for(int ele : arr){
+        //     System.out.print(ele + " ");
+        // }
+        System.out.println("The largest number is :" + getLargest(arr));
     }
 
     public static void rotate(int arr[] , int start , int end) {
@@ -25,6 +26,15 @@ public class Arrays {
             start++;
             end--;
         }
+    }
+    public static int getLargest(int arr[]){
+        int largest = arr[0];
+        for(int i = 0 ; i<arr.length ; i++){
+            if(largest<arr[i]){
+                largest = arr[i];
+            }
+        }
+        return largest;
     }
 }
 
