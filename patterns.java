5c333c241e0ac7269b629ba9b680dc9e5c0ffd00
Raman attri <raman.kumar.1232004@gmail.com>
2026-05-02 23:49:06 +0530

    Square pattern

--- patterns.java
+++ patterns.java
@@ -1,19 +1,35 @@
-import java .util.*;
-public class patterns {
-    public static void main (String args[]){
-        Scanner sc = new Scanner (System.in);
-        rectangle(sc);
-    }
+import java.util.*;
 
+public class patterns{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        rectangle(sc);
+        square(sc);
+    }
+    // print rectangle
     public static void rectangle(Scanner sc){
         int rows = sc.nextInt();
         int col = sc.nextInt();
 
-        for(int i = 1 ; i<=rows ; i++){
+        for(int i = 1 ; i<= rows ; i++){
             for(int j = 1 ; j<=col ; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    // print Square
+    public static void square (Scanner sc){
+        int rows = sc.nextInt();
+        int col = sc.nextInt();
+
+        for(int i = 1 ; i<=rows ; i++){
+            for(int j = 1 ; j<=col ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
